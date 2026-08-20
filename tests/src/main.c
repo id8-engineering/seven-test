ee677796880613eeb4fa3fb96c3b99bc0e40fb47
Bobob Bäck Engström <bobo@id8-engineering.io>
2026-08-20 15:20:36 +0200

    feat: add buzzer test
    Signed-off-by: Bobob Bäck Engström <bobo@id8-engineering.io>

--- tests/src/main.c
+++ tests/src/main.c
@@ -10,14 +10,18 @@
 #include <nrf_modem_at.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
+#include <zephyr/drivers/pwm.h>
 #include <zephyr/kernel.h>
 #include <zephyr/ztest.h>
 
 #define AT_RESPONSE_SIZE 64
+#define BUZZER_PERIOD PWM_HZ(2000)
 #define EXPECTED_MODEM_FIRMWARE "mfw_nrf91x1_2.0.4"
 
 static const struct device *led_strip =
     DEVICE_DT_GET(DT_CHOSEN(zephyr_led_strip));
+static const struct pwm_dt_spec buzzer =
+    PWM_DT_SPEC_GET(DT_NODELABEL(buzzer));
 
 ZTEST(modem, test_firmware_version) {
   int err;
@@ -65,6 +69,24 @@ ZTEST(peripherals, test_rgb_led) {
 
     err = led_strip_update_rgb(led_strip, &pixel_off, 1);
     zassert_ok(err, "Failed to turn off RGB LED");
+
+    k_msleep(100);
+  }
+}
+
+ZTEST(peripherals, test_buzzer) {
+  int err;
+
+  zassert_true(pwm_is_ready_dt(&buzzer), "Buzzer PWM device is not ready");
+
+  for (int i = 0; i < 3; i++) {
+    err = pwm_set_dt(&buzzer, BUZZER_PERIOD, BUZZER_PERIOD / 2);
+    zassert_ok(err, "Failed to turn on buzzer");
+
+    k_msleep(100);
+
+    err = pwm_set_dt(&buzzer, BUZZER_PERIOD, 0);
+    zassert_ok(err, "Failed to turn off buzzer");
 
     k_msleep(100);
   }
